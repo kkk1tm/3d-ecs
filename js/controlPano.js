6b2274b64ca6e56c1d66e1b61ab83a514291d5f0
kkk1tm <103940194+kkk1tm@users.noreply.github.com>
2023-06-28 14:24:47 +0300

    Update controlPano.js

--- js/controlPano.js
+++ js/controlPano.js
@@ -7,21 +7,21 @@ const Panorama6 = new PANOLENS.ImagePanorama('assets/Panorama_6.jpg');
 let imageContainer = document.querySelector('.panorama__container');
 
 let SpotPositions = [
-    // Связь в коридоре
+
     new THREE.Vector3(0, 0, 1500),
     new THREE.Vector3(1630, 0, -660),
-    // Связь коридора_2 и комнаты переговоров
+ 
     new THREE.Vector3(1860, 0, 620),
-    // Связь коридора_1 и комнаты переговоров
+
     new THREE.Vector3(1600, 0, 400),
     new THREE.Vector3(-1800, 0, 400),
-    // Связь переговорных
+
     new THREE.Vector3(1600, 0, 0),
     new THREE.Vector3(1800, 0, 0),
-    // Связь коридора_1 и гостинной_1
+
     new THREE.Vector3(-800, 0, -2400),
     new THREE.Vector3(0, 0, 2300),
-    // Связь гостинной
+    
     new THREE.Vector3(800, 0, 0),
     new THREE.Vector3(-800, 0, 0),
 ]
